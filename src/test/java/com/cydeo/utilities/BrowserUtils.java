b5ca3f687d0cc873bd80e5007e051f2b0329c598
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-15 14:30:05 -0400

    New cucumber project

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,4 +1,4 @@
-package com.cydeo.utility;
+package com.cydeo.utilities;
 
 import org.junit.Assert;
 
--- src/test/java/com/cydeo/utilities/ConfigurationReader.java
+++ src/test/java/com/cydeo/utilities/ConfigurationReader.java
@@ -1,4 +1,4 @@
-package com.cydeo.utility;
+package com.cydeo.utilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
