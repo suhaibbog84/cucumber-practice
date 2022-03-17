100f0956bb1e415eb3dcb336dfaed259969157c6
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-17 14:50:49 -0400

    day2 - Login Step Definition

--- src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Login_StepDefinitions.java
@@ -44,7 +44,7 @@ public class Login_StepDefinitions {
 
     @Given("user is on the library login page")
     public void user_is_on_the_library_login_page() {
-        System.out.println("user is on the library login page");
+        System.out.println("user is on the login page");
     }
 
 }
