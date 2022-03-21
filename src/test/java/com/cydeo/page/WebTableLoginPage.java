e41fbbf84f6f73ac83e3cb1d4dfdc6d99fb91e06
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-21 12:38:55 -0400

    day4 - data Table -  Web element Login page

---
+++ src/test/java/com/cydeo/page/WebTableLoginPage.java
@@ -0,0 +1,24 @@
+package com.cydeo.page;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebTableLoginPage {
+
+    public WebTableLoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(name = "username")
+    public WebElement inputUsername;
+
+    @FindBy(name = "password")
+    public WebElement inputPassword;
+
+    @FindBy(xpath = "//button[.='Login']")
+    public WebElement loginButton;
+
+}
