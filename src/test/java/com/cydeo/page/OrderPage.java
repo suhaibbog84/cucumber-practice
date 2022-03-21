6f784ee64883aa9ce1229d9a446356104582e82f
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-21 17:00:36 -0400

    day4 - Order Page

--- src/test/java/com/cydeo/page/OrderPage.java
+++ src/test/java/com/cydeo/page/OrderPage.java
@@ -14,4 +14,22 @@ public class OrderPage extends BasePage{
     @FindBy(name = "product")
     public WebElement productDropdown;
 
+    @FindBy(name = "quantity")
+    public WebElement inputQuantity;
+
+    @FindBy(name = "name")
+    public WebElement inputName;
+
+    @FindBy(name = "street")
+    public WebElement inputStreet;
+
+    @FindBy(name = "city")
+    public WebElement inputCity;
+
+    @FindBy(name = "state")
+    public WebElement inputState;
+
+    @FindBy(name = "zip")
+    public WebElement inputZip;
+
 }
