fba7b59f20b42de97edfd76c7ddfc7fbbb70ddba
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-21 14:50:33 -0400

    day4 - Data Table Step Definitions

--- src/test/java/com/cydeo/page/DropdownsPage.java
+++ src/test/java/com/cydeo/page/DropdownsPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.page;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DropdownsPage {
@@ -9,4 +11,7 @@ public class DropdownsPage {
 
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(id = "month")
+    public WebElement monthDropdown;
 }
--- src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/DataTable_StepDefinitions.java
@@ -1,9 +1,11 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.page.DropdownsPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -21,10 +23,16 @@ public class DataTable_StepDefinitions {
     public void user_is_on_the_dropdowns_page_of_practice_tool() {
         Driver.getDriver().get("https://practice.cydeo.com/dropdown");
     }
+
+    DropdownsPage dropdownsPage = new DropdownsPage();
+
     @Then("User should see below info in month dropdown")
     public void user_should_see_below_info_in_month_dropdown(List<String> expectedMonths) {
 
         System.out.println("expectedMonths = " + expectedMonths);
+
+        Select select = new Select(dropdownsPage.monthDropdown);
+
     }
 
 
