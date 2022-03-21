f5f09bdc9fbc9715f2f765d8c835d05dc0133a53
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-21 16:48:09 -0400

    day4 - Order Page

---
+++ src/test/java/com/cydeo/page/OrderPage.java
@@ -0,0 +1,14 @@
+package com.cydeo.page;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrderPage extends BasePage{
+
+    public OrderPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+
+}
