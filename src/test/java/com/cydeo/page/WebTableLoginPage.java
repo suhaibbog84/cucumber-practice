f2b144b79a038cf513f7fbe854d9880b3004eb79
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-21 12:51:09 -0400

    day4 - data Table -  Web element Login page and step definitions

--- src/test/java/com/cydeo/page/WebTableLoginPage.java
+++ src/test/java/com/cydeo/page/WebTableLoginPage.java
@@ -4,7 +4,6 @@ import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebTableLoginPage {
 
--- src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/WebTable_StepDefinitions.java
@@ -1,12 +1,16 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.page.WebTableLoginPage;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class WebTable_StepDefinitions {
+
+    WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
 
     @Given("user is on the login page of web table app")
     public void user_is_on_the_login_page_of_web_table_app() {
@@ -17,22 +21,22 @@ public class WebTable_StepDefinitions {
 
     @When("user enters username {string}")
     public void user_enters_username(String string) {
-
+        webTableLoginPage.inputUsername.sendKeys(string);
     }
 
     @When("user enters password {string}")
     public void user_enters_password(String string) {
-
+        webTableLoginPage.inputPassword.sendKeys(string);
     }
 
     @When("user click to login button")
     public void user_click_to_login_button() {
-
+        webTableLoginPage.loginButton.click();
     }
 
     @Then("user should see url contains orders")
-    public void user_should_see_url_contains_orders() {
-
+    public void user_should_see_url_contains_orders(String expectedInURL) {
+        Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains(expectedInURL));
     }
 
 
