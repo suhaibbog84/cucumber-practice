a2bdf75eef2dff2c74adf7ca1d63d2c181153022
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-22 14:51:20 -0400

    day5 - View All Order Page

--- src/test/java/com/cydeo/page/ViewAllOrderPage.java
+++ src/test/java/com/cydeo/page/ViewAllOrderPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ViewAllOrderPage {
+public class ViewAllOrderPage extends BasePage{
 
     public ViewAllOrderPage(){
         PageFactory.initElements(Driver.getDriver(), this);
