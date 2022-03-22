eb1ca8fb105916c52cd9ec9effa70f5e922c8ac6
Suhaib B <suhaib.bog84@yahoo.com>
2022-03-22 14:33:32 -0400

    day5 - BrowserUtils

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,11 +1,12 @@
 package com.cydeo.utilities;
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
